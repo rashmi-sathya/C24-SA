0c87322b32a821b3dd8d4362ee04daff278f9ac9
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-06-16 06:38:08 +0530

    Update Tower.js

--- js/Tower.js
+++ js/Tower.js
@@ -11,10 +11,8 @@ class Tower {
   }
   display() {
     var pos = this.body.position;
-    var angle = this.body.angle;
     push();
     translate(pos.x, pos.y);
-    rotate(angle);
     imageMode(CENTER);
     image(this.image, 0, 0, this.width, this.height);
     pop();
